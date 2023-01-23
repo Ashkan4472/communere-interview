ac84c5bdc794bb469dff4def262b5272d42f3a0b
Ashkan Tofangdar <ashkan4472@gmail.com>
2023-01-23 23:08:08 +0330

    Added map input component

---
+++ src/app/shared/components/inputs/map/map-input.component.ts
@@ -0,0 +1,39 @@
+import { CommonModule } from "@angular/common";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { MapComponent } from "../../map/map.component";
+import { BaseInputComponent } from "../base-input.component";
+import * as L from "leaflet";
+import { Location } from "src/app/models/location.model";
+
+@Component({
+  imports: [
+    CommonModule,
+    FormsModule,
+    BaseInputComponent,
+    MapComponent,
+  ],
+  standalone: true,
+  selector: "app-map-input",
+  templateUrl: "./map-input.component.html",
+  styleUrls: ["./map-input.component.scss"],
+})
+export class MapInputComponent {
+  @Input() value?: L.LatLngTuple;
+  @Output() valueChange = new EventEmitter<L.LatLngTuple>()
+
+  @Input() title: string = "Location on Map: "
+  @Input() id!: string;
+
+  mockLocation: Location;
+
+  constructor() {
+    this.mockLocation = new Location(this.value)
+  }
+
+  onValueChange(event: L.LatLng) {
+    this.value = [event.lat, event.lng];
+    this.valueChange.emit(this.value);
+    this.mockLocation = new Location(this.value);
+  }
+}
