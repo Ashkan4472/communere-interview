ac2e69795e67f2105623857b7f63be530d746b43
Ashkan Tofangdar <ashkan4472@gmail.com>
2023-01-24 04:23:51 +0330

    Moved map input mock location to OnInit

--- src/app/shared/components/inputs/map/map-input.component.ts
+++ src/app/shared/components/inputs/map/map-input.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from "@angular/common";
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { MapComponent } from "../../map/map.component";
 import { BaseInputComponent } from "../base-input.component";
@@ -18,16 +18,16 @@ import { Location } from "src/app/models/location.model";
   templateUrl: "./map-input.component.html",
   styleUrls: ["./map-input.component.scss"],
 })
-export class MapInputComponent {
+export class MapInputComponent implements OnInit {
   @Input() value?: L.LatLngTuple;
   @Output() valueChange = new EventEmitter<L.LatLngTuple>()
 
   @Input() title: string = "Location on Map: "
   @Input() id!: string;
 
-  mockLocation: Location;
+  mockLocation?: Location;
 
-  constructor() {
+  ngOnInit(): void {
     this.mockLocation = new Location(this.value)
   }
 
